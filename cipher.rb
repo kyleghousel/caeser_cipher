983f80ed7b3afa3729ea881f23f757ca5b70c04a
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-01-19 12:03:44 -0500

    update
    method takes string
    iterates through each char
    pulls ascii value
    skips spaces
    applies shift to value and returns respective chars
    
    needs: apply shift keeping it wrapped within the confines of uppercase and lowercase letters respectively, from z to a

--- cipher.rb
+++ cipher.rb
@@ -1,19 +1,30 @@
 def caeser_cipher(message, shift_factor)
-    i = 0
-    new_message = ""
-    while i <= shift_factor do
-        new_message += message
-        i += 1
-    end
-    p new_message
+    decipher = message.split('')
+    shifted_message = []
+    deciphered = decipher.each { |char| 
+        
+        if char == " "
+            new_char = char
+            shifted_message.push(new_char.chr)
+        else 
+            new_char = char.ord + shift_factor
+            shifted_message.push(new_char.chr)
+        end
+
+        }
+
+    shifted_message = shifted_message.join()
+    p shifted_message
+    
 end
 
 caeser_cipher("Why hello there!", 4)
 
 
 #Cipher Steps
-# 1. Capture string in an array
-# 2. Split string into characters
-# 3. Use ASCII value to apply shift
+# 1. Capture string in an array ✓
+# 2. Split string into characters ✓
+# 3. Use ASCII value to apply shift 
 # 4. Join new characters back together
 # 5. Return new string
+#6. 
